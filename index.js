2b68300eafc8cbcf4323d823b0eef04a66fda336
Geetha <geetha9876koppera@gmail.com>
2024-10-20 10:12:26 -0500

    Initial APIs and authentication changes

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const productCategories = require('./routes/productCategories');
+const products = require('./routes/products');
+const users = require('./routes/users');
+const app = express();
+const PORT = 5001;
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const orders = require('./routes/orders');
+
+app.use(cors());
+app.use(bodyparser.json());
+app.use('/productCategories', productCategories);
+app.use('/products', products);
+app.use('/users', users);
+app.use('/orders', orders);
+
+app.use('/', (req, res)=>{
+  res.send(`<h1>Hello World</h1>`)
+});
+
+const server = app.listen(PORT, () => {
+  console.log('App is running on the port - 5001');
+});
